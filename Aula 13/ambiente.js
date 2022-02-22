5a028c128c2fd47a1350f020b3bae253bb94c83a
Eduardo <edugc20142hotmail.com>
2022-02-22 14:03:03 -0300

    Estudando JavaScript 2.0

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,33 @@
+var id = 7
+
+switch (id) {
+    case id > 10: console.log ('Sim')
+    break
+    case id < 10: console.log ('Não')
+    break
+}
+
+/*
+var c = 1
+do {
+    console.log(`Passo: ${c}`)
+    c++
+}while (c <= 5)
+*/
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
